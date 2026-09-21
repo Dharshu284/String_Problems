d4d52a3cf36471e744736d63411aab5c68a49fdf
Dharsha124 <24ucs124dharsha@kgkite.ac.in>
2026-09-21 19:17:18 +0530

    ninth commit

---
+++ P9.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class P9 {
+    public static void main(String[] args) {
+        String s = "programming";
+        char target = 'm';
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == target)
+                count++;
+        }
+
+        System.out.println(count);
+    }
+}
