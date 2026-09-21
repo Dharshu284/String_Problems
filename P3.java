d62158756ed3eb8d21264d0712c4fcf0f15bb144
Dharsha124 <24ucs124dharsha@kgkite.ac.in>
2026-09-21 19:09:00 +0530

    third commit

---
+++ P3.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class P3 {
+    public static void main(String[] args) {
+        String s = "Education";
+        int count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == 'a' || c == 'e' || c == 'i' ||
+                c == 'o' || c == 'u' ||
+                c == 'A' || c == 'E' || c == 'I' ||
+                c == 'O' || c == 'U') {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
