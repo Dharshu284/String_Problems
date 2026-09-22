50672281e06d449c7d257539367a915a982e9c41
Dharsha124 <24ucs124dharsha@kgkite.ac.in>
2026-09-22 21:21:09 +0530

    eighteen commit

---
+++ P18.java
@@ -0,0 +1,10 @@
+class P18 {
+    public static void main(String[] args) {
+        String s = "I love coding";
+        String[] words = s.split(" ");
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            System.out.print(words[i] + " ");
+        }
+    }
+}
