5d66e9c689b0b9a8eec2c0f7a1ac6230ab069867
Dharsha124 <24ucs124dharsha@kgkite.ac.in>
2026-09-22 21:12:46 +0530

    twelth commit

---
+++ P12.java
@@ -0,0 +1,19 @@
+class P12 {
+    public static void main(String[] args) {
+        String s = "madam";
+        boolean palindrome = true;
+
+        int i = 0, j = s.length() - 1;
+
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                palindrome = false;
+                break;
+            }
+            i++;
+            j--;
+        }
+
+        System.out.println(palindrome);
+    }
+}
