06b45b04e48d7a580631582a6656d888f764239a
Dharsha124 <24ucs124dharsha@kgkite.ac.in>
2026-09-22 21:15:44 +0530

    fourteen commit

---
+++ P14.java
@@ -0,0 +1,10 @@
+class P14 {
+    public static void main(String[] args) {
+        String s = "hello world";
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ')
+                System.out.print(s.charAt(i));
+        }
+    }
+}
