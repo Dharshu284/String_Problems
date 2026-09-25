1036f2b247f6d7e1660b8a448902d87c162d88e4
Dharsha124 <24ucs124dharsha@kgkite.ac.in>
2026-09-25 16:12:46 +0530

    twentytwo commit

---
+++ P22.java
@@ -0,0 +1,23 @@
+class Main {
+    public static void main(String[] args) {
+        String s = "banana";
+        char result = s.charAt(0);
+        int max = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int count = 0;
+
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j))
+                    count++;
+            }
+
+            if (count > max) {
+                max = count;
+                result = s.charAt(i);
+            }
+        }
+
+        System.out.println(result);
+    }
+}
